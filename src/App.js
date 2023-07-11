a70a2216179fcd270799c0e2e37a23afb2f5fe62
hayahas <haya-nael@hotmail.com>
2023-07-11 07:02:02 +0000

    Lab16 re submission

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
 
 import Home from './components/home/Home';
-
-import db from './data/db.json';
 import './App.css';
-import Tours from './components/tours/Tours';
-
 function App() {
   return (
     <div className="App">
  <Home/>
- {
-        db.map((obj, i) => (
-          <Tours  name={obj.name} image={obj.image} />
-        ))
-      }
+ 
     </div>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,8 @@
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
 import Tours from '../tours/Tours';
+
+import db from '../../data/db.json';
 function Home(){
   
     
@@ -8,7 +10,11 @@ function Home(){
     <div>
       
     <Header/>
-    <Tours/>
+    {
+        db.map((obj, i) => (
+          <Tours  name={obj.name} image={obj.image} />
+        ))
+      }
     <Footer/>
     </div>
     );
